e6491d6793ec14e0d4cef4ff5d42e0da4c99438f
Kevin Elliott <skiimilk@gmail.com>
2023-05-02 17:02:54 +0800

    experiment with netlify function context

--- backend/api/function-one.ts
+++ backend/api/function-one.ts
@@ -2,6 +2,8 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 
 // http://localhost:8888/.netlify/functions/function-one
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  console.log(JSON.stringify({ event }));
+  console.log(JSON.stringify({ context }));
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello, world!" })
