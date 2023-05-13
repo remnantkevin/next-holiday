ea7705d9955b467f4afc760905794b6ff2a69eee
Kevin Elliott <skiimilk@gmail.com>
2023-05-13 23:08:24 +0800

    Add initial page layout with dummy data and relative date logic (#5)

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -1,49 +1,80 @@
 import "./app.css";
 
-import { useState } from "preact/hooks";
+const LOCALE = "en-AU" as const;
+const TODAY = getDayDate(new Date());
+// const pluralRules = new Intl.PluralRules(LOCALE, { type: "cardinal" });
+const fullDateFormatter = new Intl.DateTimeFormat(LOCALE, { dateStyle: "full" });
+const relativeDateFormatter = new Intl.RelativeTimeFormat(LOCALE, {
+  localeMatcher: "best fit",
+  numeric: "auto",
+  style: "long"
+});
 
-import viteLogo from "/vite.svg";
+// function pluralize(amount: number, singular: string, plural: string): string {
+//   const word = pluralRules.select(amount) === "one" ? singular : plural;
+//   return `${amount} ${word}`;
+// }
 
-import preactLogo from "./assets/preact.svg";
+// function spreadDate(date: Date): [number, number, number] {
+//   return [date.getFullYear(), date.getMonth() + 1, date.getDate()];
+// }
 
-export function App() {
-  const [count, setCount] = useState(0);
-  const [country, setCountry] = useState(undefined);
-  const [subdivision, setSubdivision] = useState(undefined);
-
-  async function getLocationData() {
-    const location = await fetch("/location").then((r) => r.json());
-    const holiday = await fetch(
-      `/.netlify/functions/holidays?countryCode=${location.countryCode}&subdivisionCode=${location.subdivisionCode}`
-    ).then((r) => r.json());
-
-    setCountry(holiday.countryCode);
-    setSubdivision(holiday.subdivisionCode);
+function getDayDate(date: Date): Date;
+function getDayDate(year: number, month: number, day: number): Date;
+function getDayDate(dateOrYear: Date | number, month?: number, day?: number): Date {
+  if (isDate(dateOrYear)) {
+    return new Date(dateOrYear.getFullYear(), dateOrYear.getMonth(), dateOrYear.getDate());
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return new Date(dateOrYear, month! - 1, day);
+}
+
+// function isNumber(v: unknown): v is number {
+//   return typeof v === "number";
+// }
+
+function isDate(v: unknown): v is Date {
+  return v instanceof Date;
+}
+
+function getFullDateStr(date: Date): string;
+function getFullDateStr(year: number, month: number, day: number): string;
+function getFullDateStr(dateOrYear: Date | number, month?: number, day?: number): string {
+  if (isDate(dateOrYear)) {
+    return fullDateFormatter.format(dateOrYear);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return fullDateFormatter.format(getDayDate(dateOrYear, month!, day!));
+}
+
+function getDistanceInDays(from: Date, to: Date): number {
+  return Math.floor((to.getTime() - from.getTime()) / 1000 / 3600 / 24);
+}
+
+function getRelativeDaysStr(from: Date, to: Date): string {
+  return relativeDateFormatter.format(getDistanceInDays(from, to), "day");
+}
+
+// TODO: Remove dummy data
+const holidayName = "Easter Monday";
+const holidayCountryName = "Australia";
+const holidaySubdivisionName = "Western Australia";
+const holidayDate = getDayDate(2023, 5, 19);
+
+export function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank" rel="noreferrer">
-          <img src={preactLogo} className="logo preact" alt="Preact logo" />
-        </a>
+        <p>{getFullDateStr(holidayDate)}</p>
+        <p>{holidayName}</p>
+        <p>{getRelativeDaysStr(TODAY, holidayDate)}</p>
       </div>
-      <h1>Vite + Preact</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <button
-          onClick={() => {
-            void getLocationData();
-          }}
-        >
-          Get location
-        </button>
-        <p>Country: {country ?? "None"}</p>
-        <p>Subdivision: {subdivision ?? "None"}</p>
-      </div>
+      <p>is the next public holiday</p>
+      <p>
+        in {holidaySubdivisionName}, {holidayCountryName}
+      </p>
     </>
   );
 }
