bd72996c85299969ad37a7a17c239076fcce0844
Kevin Elliott <skiimilk@gmail.com>
2023-09-30 12:41:24 +0800

    Remove old version files

--- data-scripts/src/shared/types.ts
+++
@@ -1,3 +0,0 @@
-export type ExpectTrue<A extends true> = A;
-
-export type IsEqual<A, B> = A extends B ? (B extends A ? true : false) : false;
--- data-scripts/src/shared/utils.ts
+++
@@ -1,9 +0,0 @@
-export function sortKeyComparator(a: { sortKey: string }, b: { sortKey: string }): 1 | -1 {
-  return a.sortKey > b.sortKey ? 1 : -1;
-}
-
-export async function fetchJSON(url: string): Promise<unknown> {
-  const response = await fetch(url);
-  const responseJSON: unknown = await response.json();
-  return responseJSON;
-}
