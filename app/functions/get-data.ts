79bab22967a07f83e560f693525fba4116a9b189
Kevin Elliott <skiimilk@gmail.com>
2023-10-25 10:47:47 +0800

    wip

---
+++ app/functions/get-data.ts
@@ -0,0 +1,26 @@
+// import { Context } from "https://edge.netlify.com";
+
+// export default (request: Request, context: Context) => {
+//   console.log(JSON.stringify({ request }));
+//   console.log(JSON.stringify({ context }));
+
+//   return Response.json({
+//     testEvent: true,
+//     countryCode: context.geo.country?.code,
+//     countryName: context.geo.country?.name,
+//     subdivisionCode: context.geo.subdivision?.code,
+//     subdivisionName: context.geo.subdivision?.name,
+//   });
+// };
+
+// interface Env {
+//   KV: KVNamespace;
+// }
+
+export const onRequest: PagesFunction = async (context) => {
+  // const value = await context.env.KV.get("example");
+
+  console.log(JSON.stringify({ context }));
+
+  return new Response("testResponse");
+};
