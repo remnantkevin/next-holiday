0f07cb732ad44acb039aea923a6043af0efd1fe1
Kevin Elliott <skiimilk@gmail.com>
2023-10-25 11:59:02 +0800

    wip

--- app/functions/get-data.ts
+++ app/functions/get-data.ts
@@ -20,12 +20,20 @@
 export const onRequest: PagesFunction = async (context) => {
   // const value = await context.env.KV.get("example");
 
-  console.log({ context });
-  console.log({ "param keys": Object.keys(context.params) });
-  console.log({ "env keys": Object.keys(context.env) });
-  console.log({ "data keys": Object.keys(context.data) });
-  console.log({ "context.request.cf": context.request.cf });
-  console.log({ "context.request.headers.entries()": Object.fromEntries(context.request.headers.entries()) });
+  // console.log({ context });
+  // console.log({ "param keys": Object.keys(context.params) });
+  // console.log({ "env keys": Object.keys(context.env) });
+  // console.log({ "data keys": Object.keys(context.data) });
+  // console.log({ "context.request.cf": context.request.cf });
+  // console.log({ "context.request.headers.entries()": Object.fromEntries(context.request.headers.entries()) });
 
-  return new Response("testResponse");
+  const countryCode = context.request.cf?.country ?? "";
+  const subdivisionCode = context.request.cf?.regionCode ?? "";
+  const subdivisionName = context.request.cf?.region ?? "";
+
+  return Response.json({
+    countryCode,
+    subdivisionCode,
+    subdivisionName,
+  });
 };
